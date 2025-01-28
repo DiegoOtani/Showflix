f42369223cf78c222d15142f5a3f9c68ca13c377
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-01-28 17:51:26 -0300

    feat: add UserRoutes on app

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
 
+const userRoutes = require('../routes/Users');
+
 const app = express();
 
 app.use(express.json());
@@ -9,5 +11,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(cors({
   origin: '*',
 }));
+
+app.use('/users', userRoutes);
 
 module.exports = app;
