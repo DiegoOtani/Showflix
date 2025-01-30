e044c165c18af766f331d06246187950e6fe0d5a
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-01-30 19:50:38 -0300

    backend/refactor: removing getUsers route and PrismaClient from User controller

--- backend/src/controllers/Users.js
+++ backend/src/controllers/Users.js
@@ -1,19 +1,7 @@
-const PrismaClient = require('@prisma/client').PrismaClient;
 const UserService = require('../services/Users');
-const prisma = new PrismaClient();
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 const bcrypt = require('bcrypt');
-
-module.exports.getUsers = async(req, res) => {
-  try {
-    const user = await prisma.user.findMany();
-    res.status(200).json(user);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Internal server error'});
-  }
-};
 
 module.exports.createUser = async(req, res) => {
   try {
--- backend/src/routes/Users.js
+++ backend/src/routes/Users.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const { getUsers, createUser, userLogin } = require('../controllers/Users');
+const { createUser, userLogin } = require('../controllers/Users');
 const { authMiddleware } = require('../middlewares/Auth');
 
-router.get('/', getUsers);
 router.post('/', createUser);
 router.post('/login', userLogin);
 
