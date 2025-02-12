39155cd760a2fa23ac830b41cd2b45fec0114400
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-12 18:13:16 -0300

    backend/feat: implement validators in routes

--- backend/src/routes/TvShows.js
+++ backend/src/routes/TvShows.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { getTvShows, createTvShow, getShowByName } = require('../controllers/TvShows');
 const { authMiddleware } = require('../middlewares/Auth');
+const validateTvShow = require('../middlewares/validateTvShow');
 
 router.get('/', getTvShows);
-router.post('/', authMiddleware, createTvShow);
+router.post('/', authMiddleware, validateTvShow, createTvShow);
 router.get('/:name', authMiddleware, getShowByName);
 
 module.exports = router;
--- backend/src/routes/Users.js
+++ backend/src/routes/Users.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { createUser, userLogin } = require('../controllers/Users');
+const validateLogin = require('../middlewares/validateLogin');
 
 router.post('/', createUser);
-router.post('/login', userLogin);
+router.post('/login', validateLogin , userLogin);
 
 module.exports = router;
