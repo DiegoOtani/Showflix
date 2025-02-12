654ef4c27655695f411820518c6bee7d273b91ab
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 21:42:16 -0300

    frontend/feat: create router, configure and use router from React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
-import { TvShows } from "./pages/Shows"
 
 function App() {
 
   return (
     <>
       <Header />
-      <TvShows />
+      <Outlet />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './Globals.css'
 import {ShowsProvider } from './contexts/TvShowsContext.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ShowsProvider>
-      <App />
+      <RouterProvider router={router}/>
     </ShowsProvider>
   </StrictMode>,
 )
