f426a689345d868590d89f45f3f5fa4fd10bcfb9
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 22:43:49 -0300

    frontend/style/: add padding to Shows component

--- frontend/src/components/Shows/styles.js
+++ frontend/src/components/Shows/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const ShowsSectionStyled = styled.section`
+  padding: 20px 200px;
   width: 100%;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
