af0e4578b2f8ffc269c6027b8d7a17d4482b6122
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 23:07:35 -0300

    frontend/feat: add UserContext and use envolving application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Globals.css'
 import {ShowsProvider } from './contexts/TvShowsContext.jsx'
+import { UserProvider } from './contexts/UserContext.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ShowsProvider>
-      <RouterProvider router={router}/>
-    </ShowsProvider>
+    <UserProvider>
+      <ShowsProvider>
+        <RouterProvider router={router}/>
+      </ShowsProvider>
+    </UserProvider>
   </StrictMode>,
 )
