aba4f9007fb6c7ca3c6b31b4558210f3c091ea29
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 22:12:09 -0300

    fronend/feat: add shows/:showName route

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { TvShows } from "./pages/Shows";
+import ShowsSearchedPage from "./pages/ShowsSearched";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         index: true,
         element: <TvShows />
       },
+      {
+        path: "shows/:showName",
+        element: <ShowsSearchedPage />
+      }
     ]
   }
 ])
