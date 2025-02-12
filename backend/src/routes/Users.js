364fd593895c5bd40db47576693a1382c9410bf3
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-12 20:51:57 -0300

    backend/feat: includ rateLimit in login route and update database

--- backend/src/routes/Users.js
+++ backend/src/routes/Users.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const { createUser, userLogin } = require('../controllers/Users');
 const validateLogin = require('../middlewares/validateLogin');
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 5,
+  message: 'Muitas tentativas de login. Tente novamente mais tarde.'
+});
 
 router.post('/', createUser);
-router.post('/login', validateLogin , userLogin);
+router.post('/login', loginLimiter, validateLogin , userLogin);
 
 module.exports = router;
