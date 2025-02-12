b441495a5f9ed2fc01b2f7360ea9028cce071b46
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 22:10:44 -0300

    frontend/refactor: use Shows component to render shows

--- frontend/src/pages/Shows/index.jsx
+++ frontend/src/pages/Shows/index.jsx
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 import { useContext } from "react";
 import { ShowsContext } from "../../contexts/TvShowsContext";
 import TvShowsService from "../../services/tvshows";
-import Card from "../../components/Card";
-import { ShowsSectionStyled } from "./styles";
+import Shows from "../../components/Shows";
 
 export const TvShows = () => {
   const { shows, setShows } = useContext(ShowsContext);
@@ -25,20 +24,6 @@ export const TvShows = () => {
   }, [shows])
 
   return (
-    <ShowsSectionStyled>
-      {shows.map(show => (
-        <Card 
-          key={show.id}
-          id={show.id}
-          title={show.title}
-          imgUrl={show.img_url}
-          language={show.language}
-          rating={show.rating}
-          selected={false}
-          onClick={() => console.log('ok')}
-        />
-      ))}
-
-    </ShowsSectionStyled>
+    <Shows shows={shows}/>
   )
 };
