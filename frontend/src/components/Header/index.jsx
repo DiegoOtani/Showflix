58b55bbeb2808436c5766b88f0e394d10cf015a3
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 21:30:15 -0300

    frontend/feat: add InputField on Header to make searchs

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { HeaderStyled } from "./styles";
+import InputField from "../InputField";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
+  const [ search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    console.log("Pesquisado");
+  };
+
   return (
     <HeaderStyled>
-      <a className="logo" href="/" target="blank">SHOWFLIX</a>
-    <nav>
-      <a href="/">Tv Shows</a>
-      <a href="#" target="blank">Add Show</a>
-      <a href="#" target="blank">About</a>
-    </nav>
+      <a className="logo" href="/">SHOWFLIX</a>
+      <InputField 
+        icon={FaSearch}
+        onChange={handleChange}
+        value={search}
+        placeholder="Searching for..."
+        onClick={handleSubmit}
+      />
+      <nav>
+        <a href="/">Tv Shows</a>
+        <a href="#" target="blank">Add Show</a>
+        <a href="#" target="blank">About</a>
+      </nav>
     </HeaderStyled>
   )
 }
