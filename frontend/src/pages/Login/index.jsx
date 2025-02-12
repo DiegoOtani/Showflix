2fab9f886a539b67685657622ebe11f57f00946f
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-02-11 22:44:14 -0300

    frontend/feat: add Login Form into LoginPage

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import InputField from "../../components/InputField/index";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
     <section>
-      LoginPage
+      <h1>SHOWFLIX</h1>
+      <form onSubmit={handleSubmit}>
+        <InputField 
+          icon={MdOutlineEmail}
+          onChange={handleEmailChange}
+          value={email}
+          placeholder={"example@email.com"}
+        />
+        <InputField 
+          icon={RiLockPasswordLine}
+          onChange={handlePasswordChange}
+          value={password}
+          placeholder={"password"}
+        />
+        <button type="submit">
+          Login
+        </button>
+      </form>
     </section>
   )
 }
